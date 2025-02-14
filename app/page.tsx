6717d7737af9ae43be1bc717b7bb57cb1cf32057
Alejandro Rios <alejo.950723@gmail.com>
2025-02-14 14:31:28 -0800

    eslint and prettier adjusted

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1>Wellcome to the world of Next js</h1>
+    <h1 className="text-3xl font-black text-light-500">
+      Wellcome to the world of Next js
+    </h1>
   );
 }
