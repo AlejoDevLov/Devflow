7d47e7349036af5440ed8a71e39e06c6fef4fc7b
Alejandro Rios <alejo.950723@gmail.com>
2025-02-15 11:43:32 -0800

    Tailwindcss theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-black text-light-500">
-      Wellcome to the world of Next js
-    </h1>
-  );
+  return <h1 className="h1-bold">Wellcome to the world of Next js</h1>;
 }
