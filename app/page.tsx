3439d98a80a3726ea0bcc46c136c89b5f0d6b98d
Alejandro Rios <alejo.950723@gmail.com>
2025-02-15 13:42:21 -0800

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1 className="h1-bold">Wellcome to the world of Next js</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Wellcome to the world of Next js (inter)</h1>;
+      <h1 className="h1-bold font-space-grotesk">
+        Wellcome to the world of Next js (space-grotesk)
+      </h1>
+      ;
+    </>
+  );
 }
